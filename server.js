2f82fdbdbcca8e681692c97827daa1c39b3db21e
Jh0nC <Jh0nC>
2024-08-06 11:11:14 -0500

    se agregan endpoints de clients, auth, sales $ employees

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const SuppliesRoutes = require('./src/routes/suppliesRoutes');
 const DevolutionsRoutes = require('./src/routes/DevolutionsRoutes');
 const ProductCategoriesRoutes = require('./src/routes/productCategoriesRoutes');
 const ProductRoutes = require('./src/routes/productRoutes');
+const clientRoutes = require('./src/routes/clientRoutes');
+const employeeRoutes = require('./src/routes/employeeRoutes');
+const saleRoutes = require('./src/routes/saleRoutes');
+const authRoutes = require('./src/routes/authRoutes');
 
 dotenv.config();
 
@@ -16,6 +20,10 @@ app.use('/supplies', SuppliesRoutes);
 app.use('/devolutions', DevolutionsRoutes);
 app.use('/productCategories', ProductCategoriesRoutes);
 app.use('/product', ProductRoutes);
+app.use('/api', clientRoutes);
+app.use('/api', employeeRoutes);
+app.use('/api', saleRoutes);
+app.use('/api', authRoutes);
 
 const PORT = process.env.SERVER_PORT || 3000;
 
